c67d5a9f2f8aaf79892b59d4f89c7982f7abae81
sifad-hossain <niaz.sifad@gmail.com>
2024-03-14 11:21:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import './App.css'
+import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
+
+function App() {
+ 
+
+  return (
+    <>      
+     <Header></Header>   
+    <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
